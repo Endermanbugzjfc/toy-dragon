52760928623844cdee171994777216d5a1ca2eb8
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-05-28 15:29:38 +0800

    Recoded the custom config struct
    - Added method utils.CustomConfig.FromServerConfig(server.Config)
    - Added function utils.CustomConfig.ToServerConfig().(server.Config)

--- utils/custom-config.go
+++ utils/custom-config.go
@@ -3,27 +3,76 @@ package utils
 import "github.com/df-mc/dragonfly/server"
 
 type CustomConfig struct {
-	SystemConfig server.Config
-	UPNPForward  bool
-	Notification struct {
-		PlayerJoin  bool
-		PlayerLeave bool
-		PlayerChat  bool
-		AlertSound  bool
-		//SavePlayerFace bool // TODO
+	server.Config
+	Network struct {
+		Address     string
+		UPNPForward bool
+	}
+	Server struct {
+		Name            string
+		MaximumPlayers  int
+		ShutdownMessage string
+		AuthEnabled     bool
+		JoinMessage     string
+		QuitMessage     string
+		Notification    struct {
+			NotifyJoin bool
+			NotifyChat bool
+			NotifyQuit bool
+		}
+	}
+	World struct {
+		Name               string
+		Folder             string
+		MaximumChunkRadius int
+		SimulationDistance int
+	}
+	Player struct {
 		FaceCacheFolder string
 	}
 }
 
+func (conf CustomConfig) ToServerConfig() server.Config {
+	sc := server.DefaultConfig()
+
+	sc.Network.Address = conf.Network.Address
+	sc.Server.Name = conf.Server.Name
+	sc.Server.MaximumPlayers = conf.Server.MaximumPlayers
+	sc.Server.ShutdownMessage = conf.Server.ShutdownMessage
+	sc.Server.AuthEnabled = conf.Server.AuthEnabled
+	sc.Server.JoinMessage = conf.Server.JoinMessage
+	sc.Server.QuitMessage = conf.Server.QuitMessage
+	sc.World.Name = conf.World.Name
+	sc.World.Folder = conf.World.Folder
+	sc.World.MaximumChunkRadius = conf.World.MaximumChunkRadius
+	sc.World.SimulationDistance = conf.World.SimulationDistance
+
+	return sc
+}
+
 func DefaultConfig() CustomConfig {
-	conf := CustomConfig{
-		SystemConfig: server.DefaultConfig(),
-		UPNPForward:  false,
-	}
-	conf.Notification.PlayerJoin = false
-	conf.Notification.PlayerLeave = false
-	conf.Notification.PlayerChat = false
-	//conf.Notification.SavePlayerFace = false
-	conf.Notification.FaceCacheFolder = "Faces"
+	conf := CustomConfig{}
+	conf.FromServerConfig(server.DefaultConfig())
+	conf.Network.UPNPForward = false
+	conf.Server.Notification.NotifyJoin = false
+	conf.Server.Notification.NotifyChat = false
+	conf.Server.Notification.NotifyQuit = false
+	conf.Player.FaceCacheFolder = "faces"
+	return conf
+}
+
+func (conf *CustomConfig) FromServerConfig(sc server.Config) *CustomConfig {
+	conf.Network.Address = sc.Network.Address
+	conf.Server.Name = sc.Server.Name
+	conf.Server.MaximumPlayers = sc.Server.MaximumPlayers
+	conf.Server.ShutdownMessage = sc.Server.ShutdownMessage
+	conf.Server.AuthEnabled = sc.Server.AuthEnabled
+	conf.Server.JoinMessage = sc.Server.JoinMessage
+	conf.Server.QuitMessage = sc.Server.QuitMessage
+	conf.World.Name = sc.World.Name
+	conf.World.Folder = sc.World.Folder
+	conf.World.MaximumChunkRadius = sc.World.MaximumChunkRadius
+	conf.World.SimulationDistance = sc.World.SimulationDistance
+
 	return conf
 }
