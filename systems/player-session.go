aeb6f01aee21aa1abd77a946e199cea02c6b9ae9
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-08-07 23:27:15 +0800

    Player sessions are no longer player-based
    - Allowing dummy / advanced sessions to be created

--- systems/player-session.go
+++ systems/player-session.go
@@ -1,12 +1,37 @@
 package systems
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
-var Sessions sync.Map
+var (
+	SessionsMu sync.RWMutex
+	Sessions   []*PlayerSession
+)
+
+func NewSession(name *string) *PlayerSession {
+	ps := &PlayerSession{
+		name:     name,
+		joinTime: time.Now(),
+	}
+	SessionsMu.Lock()
+	defer SessionsMu.Unlock()
+	Sessions = append(Sessions, ps)
+	return ps
+}
 
 type PlayerSession struct {
-	Name *string
+	name *string
 
-	JoinTime int
+	joinTime time.Time
 	Note     string
+}
+
+func (ps PlayerSession) Name() string {
+	return *ps.name
+}
+
+func (ps PlayerSession) JoinTime() time.Time {
+	return ps.joinTime
 }
