49a07fcfb8c822c93ca8193114dcc1133b24d68b
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-08-07 17:26:03 +0800

    Overview tab for control panel

--- systems/control-panel.go
+++ systems/control-panel.go
@@ -1,5 +1,23 @@
 package systems
 
-func ControlPanel() {
+import "github.com/andlabs/ui"
 
+func ControlPanel() {
+	cp := ui.NewWindow("【DragonFly CP】翡翠出品。正宗廢品", 640, 480, true)
+	cp.OnClosing(func(*ui.Window) bool {
+		ui.Quit()
+		return true
+	})
+	ui.OnShouldQuit(func() bool {
+		cp.Destroy()
+		return true
+	})
+
+	tab := ui.NewTab()
+	cp.SetChild(tab)
+
+	overview := ui.NewHorizontalBox()
+	tab.Append("Overview", overview)
+
+	cp.Show()
 }
