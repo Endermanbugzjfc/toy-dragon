2fb62c39696b8872fd75d33e5c20af9a9d5fc328
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-08-07 17:50:49 +0800

    Added the players list table
    - Renamed the overview table to players tab

--- systems/control-panel.go
+++ systems/control-panel.go
@@ -1,6 +1,9 @@
 package systems
 
-import "github.com/andlabs/ui"
+import (
+	"fmt"
+	"github.com/andlabs/ui"
+)
 
 func ControlPanel() {
 	cp := ui.NewWindow("【DragonFly CP】翡翠出品。正宗廢品", 640, 480, true)
@@ -16,8 +19,50 @@ func ControlPanel() {
 	tab := ui.NewTab()
 	cp.SetChild(tab)
 
-	overview := ui.NewHorizontalBox()
-	tab.Append("Overview", overview)
+	players := ui.NewHorizontalBox()
+	tab.Append("Players", players)
+
+	plist := ui.NewTable(&ui.TableParams{
+		Model: ui.NewTableModel(PlayerListTableModelHandler{}),
+	})
+	players.Append(plist, false)
 
 	cp.Show()
+}
+
+type PlayerListTableModelHandler struct {
+}
+
+func (h PlayerListTableModelHandler) ColumnTypes(*ui.TableModel) []ui.TableValue {
+	return []ui.TableValue{
+		ui.TableColor{},    // Row colour
+		ui.TableString(""), // Player name
+		ui.TableImage{},    // Player face
+		ui.TableString(""), // Action button
+	}
+}
+
+func (h PlayerListTableModelHandler) NumRows(*ui.TableModel) int {
+	return 0 // TODO
+}
+
+func (h PlayerListTableModelHandler) CellValue(_ *ui.TableModel, row, column int) ui.TableValue {
+	switch column {
+	case 0:
+		// Check if player is punished
+		return ui.TableColor{}
+	case 1:
+		// Return player name
+		return ui.TableString("Player")
+	case 2:
+		// Return player skin
+		return ui.TableImage{}
+	case 3:
+		return ui.TableString("...")
+	}
+	panic(fmt.Errorf("invalid table column %v, expected 0-3", row))
+}
+
+func (h PlayerListTableModelHandler) SetCellValue(_ *ui.TableModel, _, _ int, _ ui.TableValue) {
+	panic("implement me")
 }
