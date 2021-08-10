59c57a89cf009d632df52b52753dc74e2981a7ed
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2021-08-11 00:20:13 +0800

    Fixed undefined CustomConfig field in player-face.go

--- systems/player-face.go
+++ systems/player-face.go
@@ -46,5 +46,5 @@ func GetFaceFilePath(p *player.Player) string {
 	name := p.Name()
 	name = strings.ReplaceAll(name, "/", "")
 	name = strings.ReplaceAll(name, "\\", "")
-	return filepath.Join(utils.Conf.Player.FaceCacheFolder, name+".png")
+	return filepath.Join(utils.Conf.Player.Folder, name+".png") // TODO: Separated directories for player face and data, or save the face inside player data
 }
